522b3742066b92d37c0e87500e232034937de7b9
h-monn <evvelo337@gmail.com>
2018-02-03 14:59:29 -0800

    Distance between two points

--- SLOHacks2018/SLOHacks2018/Helper.swift
+++ SLOHacks2018/SLOHacks2018/Helper.swift
@@ -10,6 +10,7 @@ import Foundation
 import GLKit
 import ARKit
 import CoreLocation
+import Math
 
 class Helper{
     
@@ -17,14 +18,16 @@ class Helper{
     static var myLocation : CLLocation? = nil
     static var places : [Place]? = nil
     
-    static func createTransformationMatrix(distance : Float, azimuth : Float, floor : Int) -> matrix_float4x4 {
+    static func createTransformationMatrix(distance : Float, azimuth : Float, floor : Int) -> matrix_float4x4
+    {
         let translationMatrix = GLKMatrix4MakeTranslation(0, Float(floor * -10), -1 * distance)
         let rotationMatrix = GLKMatrix4MakeYRotation(GLKMathDegreesToRadians(360-(azimuth)))
         return float4x4((SCNMatrix4FromGLKMatrix4(GLKMatrix4Multiply(rotationMatrix, translationMatrix))))
         //How this could be employed: anchor = ARAnchor(transform: createTransformationMatrix(distance: XXX, azimuth: XXX, floor: XXX))
     }
     
-    static func calculateAzimuth(startLocationLatitude : Float, startLocationLongitude : Float, endLocationLatitude : Float, endLocationLongitude : Float) -> Float {
+    static func calculateAzimuth(startLocationLatitude : Float, startLocationLongitude : Float, endLocationLatitude : Float, endLocationLongitude : Float) -> Float
+    {
         var azimuth: Float = 0
         let lat1 = GLKMathDegreesToRadians(startLocationLatitude)
         let lon1 = GLKMathDegreesToRadians(startLocationLongitude)
@@ -39,25 +42,29 @@ class Helper{
         return azimuth
     }
     
-    static func startLocationService(delegate : CLLocationManagerDelegate){
+    static func startLocationService(delegate : CLLocationManagerDelegate)
+    {
         // For use in foreground
         locationManager.stopUpdatingLocation()
         myLocation = nil
         locationManager.requestWhenInUseAuthorization()
-        if CLLocationManager.locationServicesEnabled() {
+        if CLLocationManager.locationServicesEnabled()
+        {
             locationManager.delegate = delegate
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
     }
     
-    static func sceneViewSetup(delegate : ARSKViewDelegate, sceneView : ARSKView){
+    static func sceneViewSetup(delegate : ARSKViewDelegate, sceneView : ARSKView)
+    {
         sceneView.delegate = delegate
         sceneView.showsFPS = true
         sceneView.showsNodeCount = true
         
         // Load the SKScene from 'Scene.sks'
-        if let scene = SKScene(fileNamed: "Scene") {
+        if let scene = SKScene(fileNamed: "Scene")
+        {
             sceneView.presentScene(scene)
         }
         
@@ -68,4 +75,22 @@ class Helper{
         // Run the view's session
         sceneView.session.run(configuration)
     }
+    
+    static func calcualateLatLongDist(lat_01: Float, lon_01: Float, lat_02: Float, lon_02: Float)
+    {
+        var eRadius : Float = 6371008                           // mean volumetric radius (m) nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+        var dLat : Float    = deg2rad(lat_02-lat_01);           // Calc radians between two latidual points using the function   deg2rad [ below ]
+        var dLon : Float    = deg2rad(lon_02-lon_01);           // Same but with long
+        var a : Float       = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat_01)) * Math.cos(deg2rad(lat_02)) * Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        return d;
+    }
+    
+    static func deg2rad(deg: Float)
+    {
+        return deg * (Math.PI/180)
+    }
+
 }
