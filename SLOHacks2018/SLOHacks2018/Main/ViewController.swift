b20d7a61f3385ac3ce11821e4b5f96fa72433e06
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-02-04 07:31:04 -0800

    Added stuff

--- SLOHacks2018/SLOHacks2018/Main/ViewController.swift
+++ SLOHacks2018/SLOHacks2018/Main/ViewController.swift
@@ -14,6 +14,8 @@ import CoreLocation
 
 class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDelegate {
     
+
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var toolBar: UIToolbar!
     @IBOutlet var sceneView: ARSKView!
     
@@ -56,13 +58,59 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
     }
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        spinner.stopAnimating()
+        spinner.isHidden = true
+        sceneView.alpha = 1
         // Create and configure a node for the anchor added to the view's session.
         let temp = UIImage(named: (Helper.anchorDicked[anchor.identifier]?.getImageURL())!)
         print(Helper.anchorDicked[anchor.identifier]?.getImageURL())
-        let image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*7, height: temp!.size.height*7))
+        var image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*7, height: temp!.size.height*7))
         
         let distance = Helper.myLocation?.distance(from: CLLocation(latitude: CLLocationDegrees((Helper.anchorDicked[anchor.identifier]?.getLatitude())!), longitude: CLLocationDegrees((Helper.anchorDicked[anchor.identifier]?.getLongitude())!)))
         
+        if Float(distance!) < 30{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*7, height: temp!.size.height*7))
+
+        }
+        else if Float(distance!) < 70{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*6.75, height: temp!.size.height*6.75))
+
+        }
+        else if Float(distance!) < 120{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*6.5, height: temp!.size.height*6.5))
+
+        }
+        else if Float(distance!) < 190{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*6.25, height: temp!.size.height*6.25))
+
+        }
+        else if Float(distance!) < 230{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*6, height: temp!.size.height*6))
+
+        }
+        else if Float(distance!) < 270{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*5.9, height: temp!.size.height*5.9))
+
+        }
+        else if Float(distance!) < 300{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*5.8, height: temp!.size.height*5.8))
+
+        }
+        else if Float(distance!) < 350{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*5.7, height: temp!.size.height*5.7))
+
+        }
+        else if Float(distance!) < 400{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*5.6, height: temp!.size.height*5.6))
+
+        }
+        else if Float(distance!) < 450{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*5.5, height: temp!.size.height*5.5))
+
+        }else{
+            image = Helper.resizeImage(image: temp!, targetSize: CGSize(width: temp!.size.width*5.4, height: temp!.size.height*5.4))
+
+        }
         let spriteNode = CustomNode(texture: SKTexture(image: image), distance: Float(distance!))
         return spriteNode
     }
@@ -70,13 +118,22 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if Helper.myLocation == nil{
             Helper.myLocation = locations[0]
+            spinner.isHidden = false
+            spinner.startAnimating()
+            sceneView.backgroundColor = UIColor.black
+            sceneView.alpha = 0.8
             return
+            
         }
         let newLocation = locations[0]
         let currentLocation = Helper.myLocation
         
         let distance = newLocation.distance(from: currentLocation!)
         if distance > 15{
+            spinner.isHidden = false
+            spinner.startAnimating()
+            sceneView.backgroundColor = UIColor.black
+            sceneView.alpha = 0.8
             let configuration = ARWorldTrackingConfiguration()
             configuration.worldAlignment = .gravityAndHeading
             sceneView.session.run(configuration, options: .resetTracking)
--- SLOHacks2018/SLOHacks2018/UIObjects/CustomNode.swift
+++ SLOHacks2018/SLOHacks2018/UIObjects/CustomNode.swift
@@ -19,13 +19,24 @@ class CustomNode : SKSpriteNode {
     
     init(texture: SKTexture!, distance : Float) {
         super.init(texture: texture, color: UIColor.clear, size: texture.size())
-        self.distance = SKLabelNode(text: String(distance))
-        self.distance?.isHidden = true
+        self.distance = SKLabelNode(text: String(Int(distance)) + " m")
+        self.distance?.zPosition = 2
+        var labelPos = self.position
+        labelPos.y = CGFloat(Int(labelPos.y)+700)
+        self.distance?.position = labelPos
+        self.isUserInteractionEnabled = true
+        self.zPosition = 1
         self.addChild(self.distance!)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.distance?.isHidden = false
+        self.distance?.fontSize = 300
+        self.distance?.fontColor = UIColor.white
+        self.distance?.fontName = "ArialRoundedMTBold"
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.distance?.fontSize = 0
     }
 }
 
