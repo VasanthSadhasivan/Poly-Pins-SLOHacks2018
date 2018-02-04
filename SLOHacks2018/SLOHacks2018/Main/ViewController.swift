3462f8ece7abf345d40df5cc9d6b849c110d4c50
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-02-03 20:59:41 -0800

    ARAnchorsInitialized

--- SLOHacks2018/SLOHacks2018/Main/ViewController.swift
+++ SLOHacks2018/SLOHacks2018/Main/ViewController.swift
@@ -24,12 +24,14 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
         super.viewWillAppear(animated)
         Helper.startLocationService(delegate: self as CLLocationManagerDelegate)
         Helper.sceneViewSetup(delegate: self as ARSKViewDelegate, sceneView: sceneView)
-        Helper.getPlaces()
+        Helper.getPlaces(sceneView: sceneView)
+
     }
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
         let labelNode = SKLabelNode(text: "👾")
+        labelNode.fontSize = labelNode.fontSize*5
         labelNode.horizontalAlignmentMode = .center
         labelNode.verticalAlignmentMode = .center
 
@@ -37,6 +39,20 @@ class ViewController: UIViewController, ARSKViewDelegate, CLLocationManagerDeleg
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        Helper.myLocation = locations[0]
+        if Helper.myLocation == nil{
+            Helper.myLocation = locations[0]
+            return
+        }
+        let newLocation = locations[0]
+        let currentLocation = Helper.myLocation
+        
+        let distance = newLocation.distance(from: currentLocation!)
+        print(distance)
+        if distance > 10{
+            Helper.myLocation = newLocation
+            Helper.deleteOldAnchors(sceneView: sceneView)
+            Helper.calcARAnchors(sceneView: sceneView)
+        }
+        
     }
 }
